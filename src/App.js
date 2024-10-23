f2c2746f2d441af73b988ef8aa93a63eab186876
shiva teja <shivatejaa1905@gmail.com>
2024-10-23 16:37:36 +0530

    try refresh case-1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 import Footer from './components/Footer/Footer';
-import { Routes, Route, Navigate } from 'react-router-dom';
+// import { Routes, Route, Navigate } from 'react-router-dom';
 import React, { useContext } from 'react';
 import { AuthContext } from './components/context/AuthContext';
 import HomePage from './components/HomePage';
@@ -9,24 +9,22 @@ import Login from './components/Header/Login';
 
 
 
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   const { isLoggedIn } = useContext(AuthContext);
 
   return (
     <div className="holder">
-      
-      <Routes>
-        <Route path="/" element={isLoggedIn ? <HomePage /> : <Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={isLoggedIn ? <HomePage /> : <Navigate to="/login" />} />
-      
-       
-      </Routes>
-    
+   
+        <Routes>
+          <Route path="/" element={isLoggedIn ? <HomePage /> : <Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={isLoggedIn ? <HomePage /> : <Navigate to="/login" />} />
+        </Routes>
+     
       <Footer />
     </div>
   );
 }
-
 export default App;
